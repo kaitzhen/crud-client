42868c8a5a2e56776ca97eb943c39951cd6b534e
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-04-21 20:03:21 -0400

    display helpful msg if no students enrolled in a campus

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -8,6 +8,17 @@ import { Link } from "react-router-dom";
 // Take in props data to construct the component
 const CampusView = (props) => {
   const {campus} = props;
+
+  if(!campus.students.length) {
+    return (
+      <div>
+        <h1>{campus.name}</h1>
+        <p>{campus.address}</p>
+        <p>{campus.description}</p>
+        <p>There are no students enrolled currently.</p>
+      </div>
+    )
+  }
   
   // Render a single Campus view with list of its students
   return (
@@ -15,6 +26,8 @@ const CampusView = (props) => {
       <h1>{campus.name}</h1>
       <p>{campus.address}</p>
       <p>{campus.description}</p>
+
+      <p>Students Enrolled: {campus.students.length}</p>
       {campus.students.map( student => {
         let name = student.firstname + " " + student.lastname;
         return (
