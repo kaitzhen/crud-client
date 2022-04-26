0e06dd11aab5894d04b74ef5bc085b6d8439c61d
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-04-26 11:34:03 -0400

    validate add campus form

--- src/components/views/NewCampusView.js
+++ src/components/views/NewCampusView.js
@@ -51,17 +51,17 @@ const NewCampusView = (props) => {
           </div>
           <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
             <label style= {{color:'#11153e', fontWeight: 'bold'}}>Name: </label>
-            <input type="text" name="name" onChange ={(e) => handleChange(e)} />
+            <input type="text" name="name" required onChange ={(e) => handleChange(e)} />
             <br/>
             <br/>
 
             <label style={{color:'#11153e', fontWeight: 'bold'}}>Address: </label>
-            <input type="text" name="address" onChange={(e) => handleChange(e)} />
+            <input type="text" name="address" required onChange={(e) => handleChange(e)} />
             <br/>
             <br/>
 
             <label style={{color:'#11153e', fontWeight: 'bold'}}>Description: </label>
-            <input type="text" name="description" onChange={(e) => handleChange(e)} />
+            <input type="text" name="description" required onChange={(e) => handleChange(e)} />
             <br/>
             <br/>
 
