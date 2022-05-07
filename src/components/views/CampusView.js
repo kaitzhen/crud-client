aa01548c47b654ebb11e6120143168baed2138f8
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-05-07 16:19:57 -0400

    added styling to single campus view

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -12,19 +12,25 @@ const CampusView = (props) => {
   if(!campus.students.length) {
     return (
       <div>
-        <h1>{campus.name}</h1>
-        <img src= {campus.imageUrl} alt="campus" style={{width: '100px', height: '100px', borderRadius: 50}}></img>
-        <p>{campus.address}</p>
-        <p>{campus.description}</p>
-        <p>campus id: {campus.id}</p>
-        <Link to={`/editcampus/${campus.id}`}>
-          <button>Edit Campus</button>
-        </Link>
-        <button onClick={() => deleteCampus(campus.id)}>Delete Campus</button>
-        <p>There are no students enrolled currently.</p>
-        <Link to={`/enrollstudent/${campus.id}`}>
-          <button>Enroll New Student</button>
-        </Link>
+        <h1 style={{margin:'20px', color:'white'}}>{campus.name}</h1>
+        <div style={{backgroundColor:'white', margin:'auto', marginBottom:'40px',
+        borderRadius:5, width:'50%', padding:'15px'}}>
+
+            
+            <img src= {campus.imageUrl} alt="campus" style={{width: '300px', height: '200px'}}></img>
+            <p><span style={{fontWeight:'500'}}>Address: </span>{campus.address}</p>
+            <p><span style={{fontWeight:'500'}}>Description: </span>{campus.description}</p>
+            <Link to={`/editcampus/${campus.id}`}>
+              <button style={{margin:'15px', padding:'10px',backgroundColor:'#D0D0D0',borderRadius:10,color:'black'}}>Edit Campus Information</button>
+            </Link>
+            <button onClick={() => deleteCampus(campus.id)}
+            style={{marginBottom:'15px', padding:'10px',backgroundColor:'#585858',borderRadius:10,color:'white'}}
+            >Delete Campus</button>
+            <p style={{fontWeight:'500'}}>There are no students enrolled currently.</p>
+            <Link to={`/enrollstudent/${campus.id}`}>
+              <button style={{margin:'10px', borderRadius:10, padding:'5px'}}>Enroll New Student</button>
+            </Link>
+        </div>
         
       </div>
     )
@@ -33,31 +39,49 @@ const CampusView = (props) => {
   // Render a single Campus view with list of its students
   return (
     <div>
-      <h1>{campus.name}</h1>
-      <img src= {campus.imageUrl} alt="campus" style={{width: '100px', height: '100px', borderRadius: 50}}></img>
-      <p>{campus.address}</p>
-      <p>{campus.description}</p>
-      <p>campus id:{campus.id}</p>
-      <Link to={`/editcampus/${campus.id}`}>
-        <button>Edit Campus Information</button>
-      </Link>
-      <button onClick={() => deleteCampus(campus.id)}>Delete Campus</button>
-      <h2>Students Enrolled: {campus.students.length}</h2>
-      {campus.students.map( student => {
-        let name = student.firstname + " " + student.lastname;
-        return (
-          <div key={student.id}>
-            <Link to={`/student/${student.id}`}>
-              <h2>{name}</h2>
-            </Link>            
-            <button onClick={() => unenroll(student)}>Unenroll</button> 
-          </div>
-        );
-      })}
+      <h1 style={{margin:'20px', color:'white'}}>{campus.name}</h1>
+      <div style={{backgroundColor:'white', margin:'auto', marginBottom:'40px',
+        borderRadius:5, width:'50%', padding:'15px'}}>
+          <img src= {campus.imageUrl} alt="campus" style={{width: '300px', height: '200px'}}></img>
+          <p><span style={{fontWeight:'500'}}>Address: </span>{campus.address}</p>
+          <p><span style={{fontWeight:'500'}}>Description: </span>{campus.description}</p>
+          <Link to={`/editcampus/${campus.id}`}>
+            <button style={{margin:'15px', padding:'10px',backgroundColor:'#D0D0D0',borderRadius:10,color:'black'}}>Edit Campus Information</button>
+          </Link>
+          <button onClick={() => deleteCampus(campus.id)}
+          style={{marginBottom:'15px', padding:'10px',backgroundColor:'#585858',borderRadius:10,color:'white'}}
+          >Delete Campus</button>
+          <h3 style={{margin:'5px'}}>Total Students: {campus.students.length}</h3>
+          <table className="center" style={{backgroundColor:'white', width:'75%',marginLeft:'auto',marginRight:'auto', border:'1px solid black', borderCollapse:'collapse'}}>
+            <tr> 
+              <th style={{border:'1px solid black', borderCollapse:'collapse'}}>Student Name</th>
+            </tr>
+            {campus.students.map( student => {
+            let name = student.firstname + " " + student.lastname;
+            return (
+              <tr key={student.id}>
+                <td style={{border:'1px solid black', borderCollapse:'collapse', padding:'5px'}}>
+                  <Link to={`/student/${student.id}`}>
+                    <p style={{color:'black'}}>{name}</p>
+                  </Link> 
+                  
+                </td>    
+                <td style={{border:'1px solid black', borderCollapse:'collapse', padding:'5px'}}>
+                    <button style={{borderRadius:10}} onClick={() => unenroll(student)}>Unenroll</button>
+                </td>
+              </tr>
+            );
+            })}
 
-    <Link to={`/enrollstudent/${campus.id}`}>
-        <button>Enroll New Student</button>
-    </Link>
+          </table>
+          
+
+        <Link to={`/enrollstudent/${campus.id}`}>
+            <button style={{margin:'10px', borderRadius:10, padding:'5px'}}>Enroll New Student</button>
+        </Link>
+      </div>
+      
+      
       
     </div>
   );
